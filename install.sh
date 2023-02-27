e895eb03912527572a02312ae0b1a878b2df387d
Juan Delgado <roloenusa@msn.com>
2023-02-27 12:47:53 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,6 +48,11 @@ fi
 # Node via NVM
 mkdir ~/.nvm
 brew install nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # this loads nvm bash_completion
+
 nvm install --lts
 nvm use --lts
 
