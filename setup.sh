c2e12db752c39b90d1c94bfd523533225ff57402
Juan Delgado <roloenusa@msn.com>
2023-02-28 22:58:36 +0000

    Testing setup file

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+set -euo pipefail
+# set -x # uncomment to print all commands as they happen
+
+uname=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+if [[ ! $uname == darwin ]]; then
+  exit 0;
+fi
+
+
+###
+# System Preferences
+###
+
+# Show full paths in footer of Finder windows
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Turn off natural scrolling
+defaults write -g com.apple.swipescrolldirection -bool NO
+
+
+###
+# System Provisioning
+###
+
+# Check if $HOME/bin exists
+if [[ ! -d $HOME/bin ]]; then
+  mkdir $HOME/bin
+fi
+
+# Check if the $HOME/bin folder is in the path
+if [[ ! ":$PATH:" == *":$HOME/bin"* ]]; then
+  echo "PATH not setup correctly"
+  export PATH="$PATH:$HOME/bin"
+  echo $PATH
+fi
+
+# Check if $HOME/local exists
+if [[ ! -d $HOME/.local ]]; then
+  mkdir $HOME/.local
+fi
+
+PERMISSION="$(stat -f '%p' $HOME/.local)"
+if [ ! ${PERMISSION#??} == "777" ] && [ ! ${PERMISSION#??} == "755" ]; then
+  chmod -R 777 $HOME/.local/*
+fi
