e9dc4367b25b18c8b87c5f797b069634cb5c3cda
Juan Delgado <roloenusa@msn.com>
2023-03-01 07:51:11 +0000

    Setting up scripts chezmoi way

--- run_before_setup.sh
+++ run_before_setup.sh
@@ -2,6 +2,10 @@
 set -euo pipefail
 # set -x # uncomment to print all commands as they happen
 
+echo "==========="
+echo "= Run Before: Setup"
+echo "==========="
+
 debug="" # set to y to enable more output
 uname=$(uname -s | tr "[:upper:]" "[:lower:]")
 
@@ -40,40 +44,6 @@ if ! gh auth status 1>/dev/null 2>/dev/null; then
   fi
 fi
 ########################################## End bootstrap
-
-###
-# Development
-###
-
-# Node via NVM
-mkdir -p ~/.nvm # Create directory only if it exists
-brew install nvm
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # this loads nvm bash_completion
-
-nvm install --lts
-nvm use --lts
-
-# Better finder
-$brewinstall fd
-
-# Fuzzy Search FZF
-# Installation source: https://www.youtube.com/watch?v=1a5NiMhqAR0
-$brewinstall fzf
-
-###
-# Applications
-###
-
-brew list 1password            >/dev/null || $brewinstall --cask 1password
-brew list discord              >/dev/null || $brewinstall --cask discord
-brew list docker               >/dev/null || $brewinstall --cask docker
-brew list firefox              >/dev/null || $brewinstall --cask firefox
-brew list visual-studio-code   >/dev/null || $brewinstall --cask visual-studio-code
-brew list iterm2               >/dev/null || $brewinstall --cask iterm2
-
 
 ###
 # System Configurations
---
+++ run_onchange_install-packages.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -euo pipefail
+# set -x # uncomment to print all commands as they happen
+
+echo "==========="
+echo "= Run On Change: Install Packages"
+echo "==========="
+
+debug="" # set to y to enable more output
+uname=$(uname -s | tr "[:upper:]" "[:lower:]")
+
+brew="brew"
+brewinstall="$brew install --quiet --force"
+
+###
+# Development
+###
+
+# Node via NVM
+mkdir -p ~/.nvm # Create directory only if it exists
+brew install nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # this loads nvm bash_completion
+
+nvm install --lts
+nvm use --lts
+
+# Better finder
+$brewinstall fd
+
+# Fuzzy Search FZF
+# Installation source: https://www.youtube.com/watch?v=1a5NiMhqAR0
+$brewinstall fzf
+
+###
+# Applications
+###
+
+brew list 1password            >/dev/null || $brewinstall --cask 1password
+brew list discord              >/dev/null || $brewinstall --cask discord
+brew list docker               >/dev/null || $brewinstall --cask docker
+brew list firefox              >/dev/null || $brewinstall --cask firefox
+brew list visual-studio-code   >/dev/null || $brewinstall --cask visual-studio-code
+brew list iterm2               >/dev/null || $brewinstall --cask iterm2
