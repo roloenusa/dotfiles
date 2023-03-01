8ba6d5ed884e66baf56fd8bffe49b655a038954c
Juan Delgado <roloenusa@msn.com>
2023-03-01 08:09:42 +0000

    Remote setup test

--- setup.sh
+++ setup.sh
@@ -2,39 +2,5 @@
 set -euo pipefail
 # set -x # uncomment to print all commands as they happen
 
-uname=$(uname -s | tr "[:upper:]" "[:lower:]")
-
-if [[ ! $uname == darwin ]]; then
-  exit 0;
-fi
-
-###
-# System Preferences
-###
-
-# Show full paths in footer of Finder windows
-defaults write com.apple.finder ShowPathbar -bool true
-
-# Turn off natural scrolling
-defaults write -g com.apple.swipescrolldirection -bool NO
-
-###
-# System Provisioning
-###
-
-# Check if $HOME/bin exists
-if [[ ! -d $HOME/bin ]]; then
-  mkdir $HOME/bin
-fi
-
-# Check if the $HOME/bin folder is in the path
-if [[ ! ":$PATH:" == *":$HOME/bin"* ]]; then
-  echo "PATH not setup correctly"
-  export PATH="$PATH:$HOME/bin"
-  echo $PATH
-fi
-
-# Check if $HOME/local exists
-if [[ ! -d $HOME/.local ]]; then
-  mkdir $HOME/.local
-fi
+export PATH=$HOME/bin:$PATH
+sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply roloenusa
