09754c1ee397446662bae044e6bf3f6cf7ca632e
Juan Delgado <roloenusa@msn.com>
2025-01-17 15:12:05 -0800

    Update brew packages
    * Add fonts from casks
    * Install `asdf`
    * Remove nvm

--- scripts/brew_packages.sh
+++ scripts/brew_packages.sh
@@ -14,18 +14,11 @@ brew upgrade --quiet
 # Development
 ###
 
-# Node via NVM
-if ! brew list nvm &>/dev/null; then
-  mkdir -p ~/.nvm # Create directory only if it exists
-  brew install nvm
+brew list font-monaspace             >/dev/null || $brewinstall --cask font-monaspace
+brew list font-hack-nerd-font        >/dev/null || $brewinstall --cask font-hack-nerd-font
+brew list font-roboto-mono-nerd-font >/dev/null || $brewinstall --cask font-roboto-mono-nerd-font
 
-  export NVM_DIR="$HOME/.nvm"
-  [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh" # This loads nvm
-  [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" # this loads nvm bash_completion
-
-  nvm install --lts
-  nvm use --lts
-fi
+brew list asdf                       >/dev/null || $brewinstall asdf
 
 ###
 # Utilities
@@ -35,8 +28,6 @@ brew list fd                         >/dev/null || $brewinstall fd      # Better
 brew list fzf                        >/dev/null || $brewinstall fzf     # Fuzzy Search FZF: https://www.youtube.com/watch?v=1a5NiMhqAR0
 brew list chezmoi                    >/dev/null || $brewinstall chezmoi
 brew list dive                       >/dev/null || $brewinstall dive    # Docker image inspector
-brew list font-hack-nerd-font        >/dev/null || $brewinstall homebrew/cask-fonts/font-hack-nerd-font
-brew list font-roboto-mono-nerd-font >/dev/null || $brewinstall homebrew/cask-fonts/font-roboto-mono-nerd-font
 brew list htop                       >/dev/null || $brewinstall htop
 brew list httpie                     >/dev/null || $brewinstall httpie
 brew list ripgrep                    >/dev/null || $brewinstall ripgrep
@@ -48,17 +39,18 @@ brew list youtube-dl                 >/dev/null || $brewinstall youtube-dl
 ###
 # Languages
 ###
+
 brew list go                         >/dev/null || $brewinstall go
 
 ###
 # Applications
 ###
 
-brew list 1password          >/dev/null || $brewinstall --cask 1password
-brew list discord            >/dev/null || $brewinstall --cask discord
-brew list docker             >/dev/null || $brewinstall --cask docker
-brew list firefox            >/dev/null || $brewinstall --cask firefox
-brew list iterm2             >/dev/null || $brewinstall --cask iterm2
-brew list visual-studio-code >/dev/null || $brewinstall --cask visual-studio-code
-brew list ghostty            >/dev/null || $brewinstall --cask ghostty
-# brew list warp               >/dev/null || $brewinstall --cask warp
+brew list 1password                  >/dev/null || $brewinstall --cask 1password
+brew list discord                    >/dev/null || $brewinstall --cask discord
+brew list docker                     >/dev/null || $brewinstall --cask docker
+brew list firefox                    >/dev/null || $brewinstall --cask firefox
+brew list iterm2                     >/dev/null || $brewinstall --cask iterm2
+brew list visual-studio-code         >/dev/null || $brewinstall --cask visual-studio-code
+brew list ghostty                    >/dev/null || $brewinstall --cask ghostty
+# brew list warp                     >/dev/null || $brewinstall --cask warp
